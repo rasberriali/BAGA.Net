9db2ae65382a6383e52c8cb0d0b19442f48dc175
rasberriali <julesalysa@gmail.com>
2025-04-03 11:10:43 +0800

    deployment fixes

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const authLimiter = rateLimit({
 });
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "https://baga-net.vercel.app","baga-net-backend.vercel.app"],
+  origin: ["http://localhost:5173", "https://baga-net.vercel.app","https://baga-net-backend.vercel.app"],
   methods: ["GET", "POST", "DELETE", "PUT"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
